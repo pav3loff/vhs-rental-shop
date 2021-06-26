410aefc7e1b59262d60b0801bc38a63ad545a0ac
Mario Pavlic <886pav@gmail.com>
2021-06-26 14:47:14 +0200

    added project structure

---
+++ src/main/java/truenorth/vhsrentalshop/VhsRentalShopApplication.java
@@ -0,0 +1,13 @@
+package truenorth.vhsrentalshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhsRentalShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VhsRentalShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/truenorth/vhsrentalshop/VhsRentalShopApplicationTests.java
@@ -0,0 +1,13 @@
+package truenorth.vhsrentalshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhsRentalShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
